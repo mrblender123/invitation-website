e877c1aee42943229a2052d09ae13b1015a793f1
Baruch Cheshin <bycheshin@gmail.com>
2026-05-20 16:31:01 -0400

    Revert "Fix category pills fixed with solid background to clip scrolling cards"
    This reverts commit 1749e9f49f09d374bd43d810ff90e363d6b9c53e.

--- app/components/LandingHeader.tsx
+++ app/components/LandingHeader.tsx
@@ -185,19 +185,17 @@ export default function LandingHeader() {
         </div>
       </div>
 
-      {/* Spacer for fixed header + pills */}
-      <div style={{ height: 150 }} />
+      {/* Spacer for fixed header */}
+      <div style={{ height: 76 }} />
 
-      {/* Fixed category pills */}
+      {/* Sticky category pills */}
       <div style={{
-        position: 'fixed',
+        position: 'sticky',
         top: 64,
-        left: 0,
-        right: 0,
         zIndex: 50,
+        overflow: 'visible',
         padding: '12px 24px 16px',
         pointerEvents: 'none',
-        background: 'var(--background)',
       }}>
         <div style={{
           maxWidth: 1100,
